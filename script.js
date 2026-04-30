c17a64e10a0315bbdc28997960a0063083a343d8
Katie A <ktfalbin@gmail.com>
2026-04-30 19:42:21 -0400

    futzing with modals

--- script.js
+++ script.js
@@ -45,3 +45,25 @@ guessinggameBtn.addEventListener('click', () => {
 dadjokesBtn.addEventListener('click', () => {
   window.open('https://albinkf.github.io/dad-jokes/', '_blank');
 });
+
+// Get the modal
+var modal = document.getElementById("myModal");
+var modalImg = document.getElementById("imgmodal-contn");
+var captionText = document.getElementById("caption");
+
+// 2. Select ALL images (using a class, e.g., "modal-image")
+var images = document.querySelectorAll(".modal-image");
+
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+} 
