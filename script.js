796f7e026330dfc4fcc35930490363e5e645fca0
Katie A <ktfalbin@gmail.com>
2026-04-30 15:08:46 -0400

    add all links and buttons

--- script.js
+++ script.js
@@ -2,7 +2,15 @@
 // 
 const instagramBtn = document.getElementById('instagram-btn');
 const tumblrBtn = document.getElementById('tumblr-btn');
+//github buttons
+const stockphotoBtn = document.getElementById('stockphoto-btn');
+const lyricsearchBtn = document.getElementById('lyricsearch-btn');
+const soundboardBtn = document.getElementById('soundboard-btn');
+const musicplayerBtn = document.getElementById('musicplayer-btn');
+const dadjokesBtn = document.getElementById('dadjokes-btn');
+const guessinggameBtn = document.getElementById('guessinggame-btn');
 
+//SOCIAL MEDIA links
 // click button, open in new tab
 instagramBtn.addEventListener('click', () => {
   window.open('https://www.instagram.com/ktie_arts/', '_blank');
@@ -10,4 +18,30 @@ instagramBtn.addEventListener('click', () => {
 // click button open in new tab
 tumblrBtn.addEventListener('click', () => {
   window.open('https://www.tumblr.com/blog/ktieart', '_blank');
+});
+
+
+//GITHUB links
+stockphotoBtn.addEventListener('click', () => {
+  window.open('https://albinkf.github.io/Insect-Game', '_blank');
+});
+
+
+lyricsearchBtn.addEventListener('click', () => {
+  window.open('https://albinkf.github.io/Lyrics-Search/', '_blank');
+});
+
+soundboardBtn.addEventListener('click', () => {
+  window.open('https://albinkf.github.io/Sound-Board/', '_blank');
+});
+
+musicplayerBtn.addEventListener('click', () => {
+  window.open('https://albinkf.github.io/music-player/', '_blank');
+});
+guessinggameBtn.addEventListener('click', () => {
+  window.open('https://albinkf.github.io/Speak-Number-Guessing-Game/', '_blank');
+});
+
+dadjokesBtn.addEventListener('click', () => {
+  window.open('https://albinkf.github.io/dad-jokes/', '_blank');
 });
